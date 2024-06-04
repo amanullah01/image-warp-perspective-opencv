26537b56c0ad66bdcc251283cda0fed28f998e1a
Amanullah Aman <amancse.aiub@gmail.com>
2024-06-04 13:31:40 +0600

    image warp perspective done

--- image_click_wrap_perspective.py
+++ image_click_wrap_perspective.py
@@ -17,10 +17,26 @@ def mouseclick(event, x, y, flags, param):
         print(f"Mouse click: {counter}", circles)
 
 
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+
+
 while True:
     if counter == 4:
         width, height = 500, 500
-        pts1 = np.float32([circles[0], circles[1], circles[2], circles[3]])
+        newCircles = reorder(circles)
+        # print("Original circle is: ", circles)
+        # print("new circle is: ", newCircles)
+        pts1 = np.float32([newCircles[0], newCircles[1], newCircles[2], newCircles[3]])
         pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
         imageOutput = cv2.warpPerspective(image, matrix, (width, height))
